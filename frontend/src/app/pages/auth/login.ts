618be9bce30a0bd3d3bf5bab130dd8703eb7a82b
Turgut Ozcan <turgutozcan@gmail.com>
2026-04-13 18:06:58 +0300

    logo

--- frontend/src/app/pages/auth/login.ts
+++ frontend/src/app/pages/auth/login.ts
@@ -23,11 +23,14 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
             <div class="flex flex-col items-center justify-center">
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
-                        <div class="text-center mb-8">
-                            <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">STYS - Hoşgeldiniz</div>
-                            <span class="text-muted-color font-medium">Lütfen Giriş Yapınız</span>
-                        </div>
 
+<div class="mb-8 flex flex-col items-center">
+    <img src="logo.png" class="logo-img" style="width:200px;" />
+    <!-- <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">STYS - Hoşgeldiniz</div>-->
+    <span class="text-muted-color font-medium mt-3">
+        Lütfen Giriş Yapınız
+    </span>
+</div>
                         <div>
                             <label for="username1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Kullanıcı Adı</label>
                             <input

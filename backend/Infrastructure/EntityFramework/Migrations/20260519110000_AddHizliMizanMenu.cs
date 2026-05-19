11a1bd58cf957311d6cced4f6c5721428ddd64a8
Onder DEMIR <oender.demir@gmail.com>
2026-05-19 12:06:17 +0300

    feat: add Hizli Mizan menu item and fix Hesap Adi typo - Add migration 20260519110000_AddHizliMizanMenu: creates Hizli Mizan MenuItem under Muhasebe parent with pi pi-chart-bar icon, seeds MuhasebeFisYonetimi roles (Menu/View/Manage) assigned to Admin/TesisManager/Muhasebeci groups, all idempotent SQL. Fix Hesap Adi -> Hesap Adi in table header.

---
+++ backend/Infrastructure/EntityFramework/Migrations/20260519110000_AddHizliMizanMenu.cs
@@ -0,0 +1,157 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STYS.Infrastructure.EntityFramework.Migrations;
+
+/// <inheritdoc />
+public partial class AddHizliMizanMenu : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(
+            """
+            DECLARE @Now datetime2 = SYSUTCDATETIME();
+            DECLARE @AdminGroupId uniqueidentifier = '22222222-2222-2222-2222-222222222201';
+            DECLARE @TesisManagerGroupId uniqueidentifier = '22222222-2222-2222-2222-222222222202';
+            DECLARE @MuhasebeciGroupId uniqueidentifier;
+
+            SELECT TOP (1) @MuhasebeciGroupId = [Id] FROM [TODBase].[UserGroups] WHERE [Name] = N'MuhasebeciGrubu' AND [IsDeleted] = 0;
+
+            DECLARE @MenuRoleId uniqueidentifier;
+            DECLARE @ViewRoleId uniqueidentifier;
+            DECLARE @ManageRoleId uniqueidentifier;
+
+            -- MuhasebeFisYonetimi rolleri (Hizli Mizan ve diger muhasebe fis raporlari icin)
+            SELECT TOP (1) @MenuRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'Menu';
+            IF @MenuRoleId IS NULL
+            BEGIN
+                SET @MenuRoleId = NEWID();
+                INSERT INTO [TODBase].[Roles] ([Id], [Name], [Domain], [IsDeleted], [CreatedAt], [UpdatedAt])
+                VALUES (@MenuRoleId, N'Menu', N'MuhasebeFisYonetimi', 0, @Now, @Now);
+            END;
+
+            SELECT TOP (1) @ViewRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'View';
+            IF @ViewRoleId IS NULL
+            BEGIN
+                SET @ViewRoleId = NEWID();
+                INSERT INTO [TODBase].[Roles] ([Id], [Name], [Domain], [IsDeleted], [CreatedAt], [UpdatedAt])
+                VALUES (@ViewRoleId, N'View', N'MuhasebeFisYonetimi', 0, @Now, @Now);
+            END;
+
+            SELECT TOP (1) @ManageRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'Manage';
+            IF @ManageRoleId IS NULL
+            BEGIN
+                SET @ManageRoleId = NEWID();
+                INSERT INTO [TODBase].[Roles] ([Id], [Name], [Domain], [IsDeleted], [CreatedAt], [UpdatedAt])
+                VALUES (@ManageRoleId, N'Manage', N'MuhasebeFisYonetimi', 0, @Now, @Now);
+            END;
+
+            -- Rolleri kullanici gruplarina ata
+            IF EXISTS (SELECT 1 FROM [TODBase].[UserGroups] WHERE [Id] = @AdminGroupId)
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @AdminGroupId AND [RoleId] = @MenuRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @AdminGroupId, @MenuRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @AdminGroupId AND [RoleId] = @ViewRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @AdminGroupId, @ViewRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @AdminGroupId AND [RoleId] = @ManageRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @AdminGroupId, @ManageRoleId, 0, @Now, @Now);
+            END;
+
+            IF EXISTS (SELECT 1 FROM [TODBase].[UserGroups] WHERE [Id] = @TesisManagerGroupId)
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @TesisManagerGroupId AND [RoleId] = @MenuRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @TesisManagerGroupId, @MenuRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @TesisManagerGroupId AND [RoleId] = @ViewRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @TesisManagerGroupId, @ViewRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @TesisManagerGroupId AND [RoleId] = @ManageRoleId)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @TesisManagerGroupId, @ManageRoleId, 0, @Now, @Now);
+            END;
+
+            IF @MuhasebeciGroupId IS NOT NULL
+            BEGIN
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @MuhasebeciGroupId AND [RoleId] = @MenuRoleId AND [IsDeleted] = 0)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @MuhasebeciGroupId, @MenuRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @MuhasebeciGroupId AND [RoleId] = @ViewRoleId AND [IsDeleted] = 0)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @MuhasebeciGroupId, @ViewRoleId, 0, @Now, @Now);
+                IF NOT EXISTS (SELECT 1 FROM [TODBase].[UserGroupRoles] WHERE [UserGroupId] = @MuhasebeciGroupId AND [RoleId] = @ManageRoleId AND [IsDeleted] = 0)
+                    INSERT INTO [TODBase].[UserGroupRoles] ([Id], [UserGroupId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @MuhasebeciGroupId, @ManageRoleId, 0, @Now, @Now);
+            END;
+
+            -- Hizli Mizan MenuItem
+            DECLARE @MuhasebeRootId uniqueidentifier;
+            DECLARE @MenuItemId uniqueidentifier;
+
+            SELECT TOP (1) @MuhasebeRootId = [Id]
+            FROM [TODBase].[MenuItems]
+            WHERE [Label] = N'Muhasebe' AND [ParentId] IS NULL AND [IsDeleted] = 0;
+
+            IF @MuhasebeRootId IS NULL
+            BEGIN
+                SET @MuhasebeRootId = NEWID();
+                INSERT INTO [TODBase].[MenuItems] ([Id], [Label], [Icon], [Route], [ParentId], [MenuOrder], [IsDeleted], [CreatedAt], [UpdatedAt])
+                VALUES (@MuhasebeRootId, N'Muhasebe', N'pi pi-wallet', N'', NULL, 6, 0, @Now, @Now);
+            END;
+
+            SELECT TOP (1) @MenuItemId = [Id]
+            FROM [TODBase].[MenuItems]
+            WHERE [Route] = N'muhasebe/hizli-mizan';
+
+            IF @MenuItemId IS NULL
+            BEGIN
+                SET @MenuItemId = NEWID();
+                INSERT INTO [TODBase].[MenuItems] ([Id], [Label], [Icon], [Route], [ParentId], [MenuOrder], [IsDeleted], [CreatedAt], [UpdatedAt])
+                VALUES (@MenuItemId, N'Hizli Mizan', N'pi pi-chart-bar', N'muhasebe/hizli-mizan', @MuhasebeRootId, 12, 0, @Now, @Now);
+            END
+            ELSE
+            BEGIN
+                UPDATE [TODBase].[MenuItems]
+                SET [Label] = N'Hizli Mizan',
+                    [Icon] = N'pi pi-chart-bar',
+                    [ParentId] = @MuhasebeRootId,
+                    [MenuOrder] = 12,
+                    [IsDeleted] = 0,
+                    [DeletedAt] = NULL,
+                    [UpdatedAt] = @Now
+                WHERE [Id] = @MenuItemId;
+            END;
+
+            -- MenuItemRoles: Muhasebe root ve Hizli Mizan icin Menu rolu atamasi
+            IF NOT EXISTS (SELECT 1 FROM [TODBase].[MenuItemRoles] WHERE [MenuItemId] = @MuhasebeRootId AND [RoleId] = @MenuRoleId)
+                INSERT INTO [TODBase].[MenuItemRoles] ([Id], [MenuItemId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @MuhasebeRootId, @MenuRoleId, 0, @Now, @Now);
+
+            IF NOT EXISTS (SELECT 1 FROM [TODBase].[MenuItemRoles] WHERE [MenuItemId] = @MenuItemId AND [RoleId] = @MenuRoleId)
+                INSERT INTO [TODBase].[MenuItemRoles] ([Id], [MenuItemId], [RoleId], [IsDeleted], [CreatedAt], [UpdatedAt]) VALUES (NEWID(), @MenuItemId, @MenuRoleId, 0, @Now, @Now);
+            """);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(
+            """
+            DELETE mir
+            FROM [TODBase].[MenuItemRoles] mir
+            INNER JOIN [TODBase].[MenuItems] mi ON mi.[Id] = mir.[MenuItemId]
+            WHERE mi.[Route] = N'muhasebe/hizli-mizan';
+
+            DELETE FROM [TODBase].[MenuItems]
+            WHERE [Route] = N'muhasebe/hizli-mizan';
+
+            -- MuhasebeFisYonetimi rollerini silme (diger muhasebe fis raporlari da kullandigi icin
+            -- sadece bu migrasyon ile eklenen UserGroupRoles baglantilarini temizliyoruz)
+            DECLARE @MenuRoleId uniqueidentifier;
+            DECLARE @ViewRoleId uniqueidentifier;
+            DECLARE @ManageRoleId uniqueidentifier;
+
+            SELECT TOP (1) @MenuRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'Menu';
+            SELECT TOP (1) @ViewRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'View';
+            SELECT TOP (1) @ManageRoleId = [Id] FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi' AND [Name] = N'Manage';
+
+            DELETE FROM [TODBase].[UserGroupRoles] WHERE [RoleId] IN (@MenuRoleId, @ViewRoleId, @ManageRoleId);
+            DELETE FROM [TODBase].[MenuItemRoles] WHERE [RoleId] IN (@MenuRoleId, @ViewRoleId, @ManageRoleId);
+            DELETE FROM [TODBase].[Roles] WHERE [Domain] = N'MuhasebeFisYonetimi';
+            """);
+    }
+}
